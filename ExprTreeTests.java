6b30eeebd967249967fb61b0e1c40e70b156588c
Kyle Rygiel <kbar41566@gmail.com>
2019-11-20 13:23:24 -0500

    first commit for branch, added starter files including tests

---
+++ ExprTreeTests.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExprTreeTests {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+}
