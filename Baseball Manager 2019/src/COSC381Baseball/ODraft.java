66c91b6caa778f6932e1eeb523a0621bb48f2591
couryp2000 <pcoury@emich.edu>
2019-11-25 17:48:48 -0500

    Removed other workspace

--- Baseball Manager 2019/src/COSC381Baseball/ODraft.java
+++
@@ -1,9 +0,0 @@
-package COSC381Baseball;
-
-public class ODraft {
-	
-	public ODraft(Player player, Member member) {
-		member.playerList.addPlayer(player);
-	}
-
-}
--- Baseball Manager 2019/src/COSC381Baseball/STARS.java
+++
@@ -1,9 +0,0 @@
-package COSC381Baseball;
-
-public class STARS {
-	
-	public STARS(Member member) {
-		System.out.print(member.playerList);
-	}
-
-}
